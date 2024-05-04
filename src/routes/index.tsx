94e54bd80b4151217737711672f21883935bc5dd
unknown <slw_df@163.com>
2024-05-04 21:40:51 +0800

    第一次提交

---
+++ src/routes/index.tsx
@@ -0,0 +1,27 @@
+import { component$ } from "@builder.io/qwik";
+import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
+import { log } from "console";
+
+export const useVenue = routeLoader$(async () => {
+  const res = await fetch('https://first-deploy-fsxwnyt-shawn-lee.globeapp.dev/');  
+  return res.json() ;
+});
+
+export default component$(() => {
+  const signal = useVenue() ;
+  return (
+    <>
+      <h1>{signal.value.data}</h1>
+    </>
+  )
+})
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
